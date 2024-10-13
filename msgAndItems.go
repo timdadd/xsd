841e9468436b125d85af5c4482acac1414074457
TimDadd <tim.dadd@commoss.com>
2024-10-13 14:11:28 +0800

    Message & Items Function and associated tests

--- msgAndItems.go
+++ msgAndItems.go
@@ -7,31 +7,42 @@ import (
 )
 
 type Message struct {
-	Package      string         `json:"package,omitempty"`
-	Name         string         `json:"name,omitempty"`
-	MessageItems []*MessageItem `json:"messageItems,omitempty"`
-	Description  string         `json:"description,omitempty"`
-	IsNamed      bool           `json:"isNamed,omitempty"`
-	sequence     int
+	Package       string         `json:"package,omitempty"`
+	Name          string         `json:"name,omitempty"`
+	MessageItems  []*MessageItem `json:"messageItems,omitempty"`
+	Description   string         `json:"description,omitempty"`
+	IsRootMessage bool           `json:"isNamed,omitempty"` // If set to true then this is a root level message and not a sub message
+	sequence      int
 }
 
 type MessageItem struct {
 	Name              string   `json:"name,omitempty"`
 	Type              string   `json:"type,omitempty"`
+	Format            string   `json:"format,omitempty"`
 	Repeated          bool     `json:"repeated,omitempty"`
 	MandatoryOptional string   `json:"mandatoryOptional,omitempty"` // M=Mandatory,O=Optional, blank=don't know
 	MinOccurs         string   `json:"minOccurs,omitempty"`
 	MaxOccurs         string   `json:"maxOccurs,omitempty"`
 	Description       string   `json:"description,omitempty"`
 	Values            []string `json:"values,omitempty"`
-	Pattern           string   `json:"pattern,omitempty"`
 	MinInclusive      string   `json:"minInclusive,omitempty"`
 	MaxInclusive      string   `json:"maxInclusive,omitempty"`
 }
 
+type tf struct {
+	t            string // type
+	f            string // format
+	minInclusive string
+	maxInclusive string
+}
+
+var xsdTransMap map[string]tf
+
 // Messages returns messages and message items (protobuf style)
 // Could also align to json schema some time
-func (xsd *XSD) Messages() (messages []*Message, err error) {
+// fmtStd is the format standard can be "protobuf" or "json"
+func (xsd *XSD) Messages(fmtStd string) (messages []*Message, err error) {
+	buildXsdTransMap(fmtStd)
 	messageMap := make(map[string]*Message)
 	fDisplay := func(xe XsdElement, h interface{}) (interface{}, error) {
 		var currentMsg *Message
@@ -49,7 +60,7 @@ func (xsd *XSD) Messages() (messages []*Message, err error) {
 				msg.Name = currentMsg.MessageItems[len(currentMsg.MessageItems)-1].Name
 			} else {
 				msg.Name = t.Name
-				msg.IsNamed = true
+				msg.IsRootMessage = currentMsg == nil
 			}
 			if _, inMap := messageMap[msg.Name]; !inMap {
 				messageMap[msg.Name] = msg
@@ -70,7 +81,7 @@ func (xsd *XSD) Messages() (messages []*Message, err error) {
 				msg.Name = currentMsg.MessageItems[len(currentMsg.MessageItems)-1].Name
 			} else {
 				msg.Name = t.Name
-				msg.IsNamed = true
+				msg.IsRootMessage = currentMsg == nil
 			}
 			if _, inMap := messageMap[msg.Name]; !inMap {
 				messageMap[msg.Name] = msg
@@ -84,7 +95,7 @@ func (xsd *XSD) Messages() (messages []*Message, err error) {
 			if t.Ref > "" {
 				mi.Name, mi.Type = t.Ref, t.Ref
 			} else {
-				mi.setTypeOrMessage(t.Type, messageMap)
+				mi.setTypeOrMessage(mi.Type, messageMap)
 			}
 			currentMsg.MessageItems = append(currentMsg.MessageItems, mi)
 
@@ -111,7 +122,7 @@ func (xsd *XSD) Messages() (messages []*Message, err error) {
 
 		case *Element:
 			if currentMsg == nil {
-				currentMsg = &Message{sequence: len(messageMap), Name: t.Name, IsNamed: t.Name > ""}
+				currentMsg = &Message{sequence: len(messageMap), Name: t.Name, IsRootMessage: t.Name > ""}
 				if _, inMap := messageMap[currentMsg.Name]; !inMap {
 					messageMap[currentMsg.Name] = currentMsg
 				}
@@ -150,7 +161,7 @@ func (xsd *XSD) Messages() (messages []*Message, err error) {
 			if len(currentMsg.MessageItems) == 0 {
 				return currentMsg, fmt.Errorf("pattern but no current message item")
 			} else {
-				currentMsg.MessageItems[len(currentMsg.MessageItems)-1].Pattern = t.Value
+				currentMsg.MessageItems[len(currentMsg.MessageItems)-1].Format = t.Value
 			}
 
 		case *MinInclusive:
@@ -191,101 +202,98 @@ func (xsd *XSD) Messages() (messages []*Message, err error) {
 		if messages[i].Package != messages[j].Package {
 			return messages[i].Package < messages[j].Package
 		}
-		if messages[i].IsNamed != messages[j].IsNamed {
-			return messages[i].IsNamed
+		if messages[i].IsRootMessage != messages[j].IsRootMessage {
+			return messages[i].IsRootMessage
 		}
 		return messages[i].sequence < messages[j].sequence
 	})
 	return messages, nil
 }
 
+// setTypeOrMessages tries to convert internal xsd Types to other message types
+// This relies upon xsdTransMap being set
 func (mi *MessageItem) setTypeOrMessage(t string, messageMap map[string]*Message) {
-	switch t {
-	case "xs:string", "xs:normalizedString", "xs:token":
-		mi.Type = "string"
-	case "xs:long":
-		mi.Type = "int64"
-	case "xs:int", "xs:integer":
-		mi.Type = "int64"
-	case "xs:float":
-		mi.Type = "float"
-	case "xs:double":
-		mi.Type = "double"
-	case "xs:boolean":
-		mi.Type = "bool"
-	case "xs:date", "xs:datetime", "xs:time":
-		mi.Type = "google.protobuf.Timestamp"
-	case "duration":
-		mi.Type = "google.protobuf.Duration"
-	default: // This must be a message
-		if t > "" {
+	if t == "" {
+		mi.Type = mi.Name
+	} else {
+		var inMap bool
+		var typeFmt tf
+		if typeFmt, inMap = xsdTransMap[t]; inMap {
+			if typeFmt.t > "" {
+				mi.Type = typeFmt.t
+			}
+			if typeFmt.f > "" {
+				mi.Format = typeFmt.f
+			}
+			if typeFmt.minInclusive > "" {
+				mi.MinInclusive = typeFmt.minInclusive
+			}
+			if typeFmt.maxInclusive > "" {
+				mi.MaxInclusive = typeFmt.maxInclusive
+			}
+		} else {
 			// Is this a Package:Message field
 			if pkgMsg := strings.Split(t, ":"); len(pkgMsg) > 1 {
 				mi.Type = strings.ReplaceAll(t, ":", ".")
 				if len(pkgMsg) == 2 {
 					if _, inMap := messageMap[mi.Type]; !inMap {
 						messageMap[mi.Type] = &Message{
-							Package:      pkgMsg[0],
-							Name:         pkgMsg[1],
-							MessageItems: nil,
-							Description:  "",
-							IsNamed:      false,
-							sequence:     len(messageMap) + 1,
+							Package:       pkgMsg[0],
+							Name:          pkgMsg[1],
+							MessageItems:  nil,
+							Description:   "",
+							IsRootMessage: false,
+							sequence:      len(messageMap) + 1,
 						}
 					}
 				}
 			} else {
 				mi.Type = t
 			}
-		} else {
-			mi.Type = mi.Name
 		}
+	}
+}
 
+func buildXsdTransMap(formatStandard string) {
+	switch formatStandard {
+	case "protobuf":
+		xsdTransMap = map[string]tf{
+			"xs:string":           {t: "string"},
+			"xs:normalizedString": {t: "string"},
+			"xs:token":            {t: "string"},
+			"xs:long":             {t: "int64"},
+			"xs:int":              {t: "int64"},
+			"xs:integer":          {t: "int64"},
+			"xs:positiveInteger":  {t: "int64", minInclusive: "0"},
+			"xs:float":            {t: "float"},
+			"xs:decimal":          {t: "float"},
+			"xs:double":           {t: "double"},
+			"xs:boolean":          {t: "bool"},
+			"xs:date":             {t: "google.protobuf.Timestamp"},
+			"xs:datetime":         {t: "google.protobuf.Timestamp"},
+			"xs:time":             {t: "google.protobuf.Timestamp"},
+			"duration":            {t: "google.protobuf.Duration"},
+		}
+	case "json":
+		xsdTransMap = map[string]tf{
+			"xs:string":           {t: "string"},
+			"xs:normalizedString": {t: "string"},
+			"xs:token":            {t: "string"},
+			"xs:long":             {t: "integer"},
+			"xs:int":              {t: "integer"},
+			"xs:integer":          {t: "integer"},
+			"xs:positiveInteger":  {t: "integer", minInclusive: "0"},
+			"xs:float":            {t: "number"},
+			"xs:decimal":          {t: "number"},
+			"xs:double":           {t: "number"},
+			"xs:boolean":          {t: "boolean"},
+			"xs:date":             {t: "string", f: "2018-11-13"},                //New in draft 7 Date
+			"xs:datetime":         {t: "string", f: "2018-11-13T20:20:39+00:00"}, // Date and time together
+			"xs:time":             {t: "string", f: "20:20:39+00:00"},            // New in draft 7 Time0
+			"duration":            {t: "string"},                                 //New in draft 2019-09 A duration as defined by the ISO 8601 ABNF for "duration". For example, P3D expresses a duration of 3 days
+		}
+	default:
+		xsdTransMap = map[string]tf{} // No translation
 	}
 
 }
-
-// This maps to protobuf types
-// 			switch t.Type {
-//			case "xs:string", "xs:normalizedString", "xs:token":
-//				mi.Type = "string"
-//			case "xs:long":
-//				mi.Type = "sint64"
-//			case "xs:int", "xs:integer":
-//				mi.Type = "int64"
-//			case "xs:float":
-//				mi.Type = "float"
-//			case "xs:double":
-//				mi.Type = "double"
-//			case "xs:boolean":
-//				mi.Type = "bool"
-//			case "xs:date", "xs:datetime", "xs:time":
-//				mi.Type = "google.protobuf.Timestamp"
-//			case "duration":
-//				mi.Type = "google.protobuf.Duration"
-//			default: // This must be a message
-//				if t.Type > "" {
-//					// Is this a Package:Message field
-//					if strings.Contains(t.Type, ":") {
-//						mi.Type = strings.ReplaceAll(t.Type, ":", ".")
-//						pkgMsg := strings.Split(t.Type, ":")
-//						if len(pkgMsg) == 2 {
-//							if _, inMap := messageMap[mi.Type]; !inMap {
-//								messageMap[mi.Type] = &Message{
-//									Package:      pkgMsg[0],
-//									Name:         pkgMsg[1],
-//									MessageItems: nil,
-//									Description:      "",
-//									IsNamed:      false,
-//									sequence:     len(messageMap) + 1,
-//								}
-//							}
-//						}
-//					} else {
-//						mi.Type = t.Type
-//					}
-//				} else {
-//					mi.Type = t.Name
-//				}
-//
-//			}
